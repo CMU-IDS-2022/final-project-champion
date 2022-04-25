b32280264e7150a61a3d6933f149685786c5eb90
Hyungil Ed Kim <hyungil.ed.kim@gmail.com>
2022-04-25 00:26:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -120,160 +120,20 @@ page_selection = st.sidebar.radio(
 # Visualization Dashboard
 if page_selection == "US National Wide":
     st.subheader("Select Metrics to view")
-    metric_selection = st.radio(
+    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        metric_selection = st.radio(
         'View Metric',
         ('Rental Price','Crime')
-    )
-    year_list = list(top10_rental_sum['Year'].unique())
-    year_selection = st.selectbox(
-        'Year', year_list
-    )
+        )
+    with col2:
+        year_list = list(top10_rental_sum['Year'].unique())
+        year_selection = st.selectbox(
+        'Year', year_list,
+        index=4
+        )
     
-    if metric_selection=="Rental Price":
-        roomtypes = list(top10_rental_sum['Room Type'].unique())
-        roomtype_selection = st.selectbox(
-            'Room Type', roomtypes
-        )
-        
-        city_selection = alt.selection_single(nearest=True)
-        df = top10_rental_sum[(top10_rental_sum['Room Type']==roomtype_selection) & (top10_rental_sum['Year']==year_selection)]
-        background = alt.Chart(states).mark_geoshape(
-            fill='lightgray',
-            stroke='white'
-        ).project('albersUsa')
-        # background
-        cities = alt.Chart(df).encode(
-            longitude='longitude:Q',
-            latitude='latitude:Q',   
-        )
-        text = cities.mark_text(dx=0,dy=20, align='center').encode(
-            alt.Text('City', type='nominal'),
-            # opacity=alt.condition(~hover, alt.value(0), alt.value(1))
-        )
-        point = cities.mark_point(filled=True).encode(
-            size=alt.Size('RentalPrice:Q',
-                        scale=alt.Scale(range=[0,1000])
-                        ),
-            # size=alt.Size(10),
-            color=alt.condition(city_selection,'RentalPrice:Q',alt.value('gray'),type="quantitative",
-                            scale=alt.Scale(scheme='yelloworangered',),
-                            legend=alt.Legend(orient="bottom-right", title=None)),
-            tooltip=[
-            'City',
-            'Year',
-            'Room Type',
-            alt.Tooltip('RentalPrice:Q',format='$,d')
-            ],
-                            
-        ).add_selection(
-            city_selection
-        )
-        graph_rentalprice=(background+point+text).properties(
-            width=650,
-            height=400,
-            title="US City Average Rental Price of "+roomtype_selection+" at "+str(year_selection))
-        st.write(graph_rentalprice)
-    
-        city_selection="New York"
-        
-        df = top10_rental_graph[(top10_rental_graph['City']==city_selection)&(top10_rental_graph['Year']==year_selection)]
-        main=alt.Chart(df).mark_bar().encode(
-            y=alt.Y("RoomType:O", sort='y',axis=alt.Axis(ticks=False, domain=False,offset=5,title=None,labelFontStyle='bold')),
-            # y=alt.Y("CrimeType:N",sort='-x',axis=alt.Axis(title=None,)),
-            x=alt.X("mean(RentalPrice)",axis=None),
-            color=alt.Color("mean(RentalPrice):Q", sort='x',
-                            scale=alt.Scale(scheme='goldred'),
-                            legend=None),   
-        )
-        bars=main
-        text_num = main.mark_text(
-            align='left',
-            baseline='middle',
-            fontStyle='bold',
-            color='black',
-            dx=5,
-        ).encode(
-            # x=alt.value(5),
-            # text='RentalPrice',
-            text=alt.Text('mean(RentalPrice):Q',format='$,d'),
-            color=alt.Color()
-        )
-        graph_rentalprice_detail=(bars+text_num).properties(
-            width=300,
-            height=150,
-        )
-        st.write(graph_rentalprice_detail)
-        
-    elif metric_selection=="Crime":
-        # US states background
-        city_selection = alt.selection_single(nearest=True)
-        df = top10_stats_final[top10_stats_final['Year']==year_selection]
-        background = alt.Chart(states).mark_geoshape(
-            fill='lightgray',
-            stroke='white'
-        ).properties(
-            width=650,
-            height=400,
-            title="US City Average Crime Score at "+str(year_selection)
-        ).project('albersUsa')
-        # background
-        cities = alt.Chart(df).encode(
-            longitude='longitude:Q',
-            latitude='latitude:Q',   
-        )
-        text = cities.mark_text(dx=0,dy=20, align='center').encode(
-            alt.Text('City', type='nominal'),
-            # opacity=alt.condition(~hover, alt.value(0), alt.value(1))
-        )
-        point = cities.mark_point(filled=True).encode(
-            tooltip=[
-            'City',
-            'Year',
-            alt.Tooltip('Crime Score:Q',format='.1f')
-            ],
-            size=alt.Size('Crime Score',type="quantitative",
-                        scale=alt.Scale(range=[0,1000])),
-            # size=alt.Size(10),
-            color=alt.condition(city_selection,'Crime Score',alt.value('gray'),
-                            scale=alt.Scale(scheme='yelloworangered',),
-                            legend=alt.Legend(orient="bottom-right", title=None)), 
-        ).add_selection(
-            city_selection
-        )
-        graph_crime = background+point+text
-        st.write(graph_crime)
-        
-        # draw crime detail
-        city_selection = "New York"
-        df = top10_crime_stats_graph[(top10_crime_stats_graph['City']==city_selection)&(top10_crime_stats_graph['Year']==year_selection)]
-        bars=alt.Chart(df).mark_bar().encode(
-            y=alt.Y("CrimeType:N", sort='y',axis=alt.Axis(ticks=False, domain=False,offset=5,title=None,labelFontStyle='bold')),
-            # y=alt.Y("CrimeType:N",sort='-x',axis=alt.Axis(title=None,)),
-            x=alt.X("Rate",axis=None),
-            color=alt.Color("Rate:Q", sort='x',
-                            scale=alt.Scale(scheme='goldred'),
-                            legend=None),   
-        ).properties(
-            width=140,
-            height=120,
-        ).add_selection(
-            # city_selection, year_selection
-        )
-        text_num = bars.mark_text(
-            align='left',
-            baseline='middle',
-            fontStyle='bold',
-            color='black',
-            dx=5,
-        ).encode(
-            # x=alt.value(5),
-            # text='RentalPrice',
-            text='label:N',
-            color=alt.Color()
-        ).transform_calculate(label=f'format(datum.Rate,"d")')
-        graph_crime_detail = bars+text_num
-        st.write(graph_crime_detail)
-        
     
     # median income for all cities
     df = top10_stats_final[top10_stats_final['Year']==year_selection]
@@ -290,8 +150,8 @@ if page_selection == "US National Wide":
     # ).transform_filter(
         # alt.datum.rank<=5
     ).properties(
-        width=250,
-        height=250
+        width=200,
+        height=350
     )
     text_city=bars.mark_text(
         align='left',
@@ -315,8 +175,173 @@ if page_selection == "US National Wide":
         text='label:N',
         color=alt.Color()
     ).transform_calculate(label=f'format(datum.MedianIncome,"$,")')
+    
     graph_medianincome=bars+text_city+text_num
-    st.write(graph_medianincome)
+    
+    if metric_selection=="Rental Price":
+        with col3:
+            roomtypes = list(top10_rental_sum['Room Type'].unique())
+            roomtype_selection = st.selectbox(
+                'Room Type', roomtypes
+            )
+        st.subheader("US City Average Rental Price of "+roomtype_selection+" at "+str(year_selection))
+        city_selection = alt.selection_single(nearest=True, fields=['City'], empty='all')
+        df = top10_rental_sum[(top10_rental_sum['Room Type']==roomtype_selection) & (top10_rental_sum['Year']==year_selection)]
+        background = alt.Chart(states).mark_geoshape(
+            fill='lightgray',
+            stroke='white'
+        ).project('albersUsa')
+        # background
+        cities = alt.Chart(df).encode(
+            longitude='longitude:Q',
+            latitude='latitude:Q',   
+        )
+        text = cities.mark_text(dx=0,dy=20, align='center').encode(
+            alt.Text('City', type='nominal'),
+            # opacity=alt.condition(~hover, alt.value(0), alt.value(1))
+        )
+        point = cities.mark_point(filled=True).encode(
+            size=alt.Size('RentalPrice:Q',
+                        scale=alt.Scale(range=[0,1000]),
+                        legend=None
+                        ),
+            # size=alt.Size(10),
+            color=alt.condition(city_selection,'RentalPrice:Q',alt.value('gray'),type="quantitative",
+                            scale=alt.Scale(scheme='yelloworangered',),
+                            legend=None),
+            tooltip=[
+            'City',
+            'Year',
+            'Room Type',
+            alt.Tooltip('RentalPrice:Q',format='$,d')
+            ],
+                            
+        ).add_selection(
+            city_selection
+        )
+        graph_rentalprice=(background+point+text).properties(
+            width=550,
+            height=350,
+            )
+        # show Map + median income graph
+        st.write(graph_rentalprice | graph_medianincome)
+        
+        
+        
+    elif metric_selection=="Crime":
+        st.subheader("US City Average Crime Score at "+str(year_selection))
+        # US states background
+        city_selection = alt.selection_single(nearest=True)
+        df = top10_stats_final[top10_stats_final['Year']==year_selection]
+        background = alt.Chart(states).mark_geoshape(
+            fill='lightgray',
+            stroke='white'
+        ).properties(
+            width=600,
+            height=350,
+        ).project('albersUsa')
+        # background
+        cities = alt.Chart(df).encode(
+            longitude='longitude:Q',
+            latitude='latitude:Q',   
+        )
+        text = cities.mark_text(dx=0,dy=20, align='center').encode(
+            alt.Text('City', type='nominal'),
+            # opacity=alt.condition(~hover, alt.value(0), alt.value(1))
+        )
+        point = cities.mark_point(filled=True).encode(
+            tooltip=[
+            'City',
+            'Year',
+            alt.Tooltip('Crime Score:Q',format='.1f')
+            ],
+            size=alt.Size('Crime Score',type="quantitative",
+                        scale=alt.Scale(range=[0,1000]),
+                        legend=None),
+            # size=alt.Size(10),
+            color=alt.condition(city_selection,'Crime Score',alt.value('gray'),
+                            scale=alt.Scale(scheme='yelloworangered',),
+                            legend=None), 
+        ).add_selection(
+            city_selection
+        )
+        graph_crime = background+point+text
+        st.write(graph_crime | graph_medianincome)
+        
+        
+        
+    # show selected city's room type graph
+    city_list = list(top10_rental_sum['City'].unique())
+    city_selection = st.selectbox(
+    'City', city_list,
+    index=len(city_list)-4
+    )
+    
+    st.text(city_selection)
+    
+    
+    df = top10_rental_graph[(top10_rental_graph['City']==city_selection)&(top10_rental_graph['Year']==year_selection)]
+    main=alt.Chart(df).mark_bar().encode(
+        x=alt.X("RoomType:O", sort='y',axis=alt.Axis(ticks=False, domain=False,
+                                                     labelAngle=-45,
+                                                     offset=5,title=None,labelFontStyle='bold')),
+        # y=alt.Y("CrimeType:N",sort='-x',axis=alt.Axis(title=None,)),
+        y=alt.Y("mean(RentalPrice)",axis=None),
+        color=alt.Color("mean(RentalPrice):Q", sort='y',
+                        scale=alt.Scale(scheme='goldred'),
+                        legend=None),   
+    )
+    bars=main
+    text_num = main.mark_text(
+        align='center',
+        baseline='middle',
+        fontStyle='bold',
+        color='black',
+        dy=10,
+    ).encode(
+        # x=alt.value(5),
+        # text='RentalPrice',
+        text=alt.Text('mean(RentalPrice):Q',format='$,d'),
+        color=alt.Color()
+    )
+    graph_rentalprice_detail=(bars+text_num).properties(
+        width=350,
+        height=280,
+    )
+    # draw crime detail
+    df = top10_crime_stats_graph[(top10_crime_stats_graph['City']==city_selection)&(top10_crime_stats_graph['Year']==year_selection)]
+    bars=alt.Chart(df).mark_bar().encode(
+        x=alt.X("CrimeType:N", sort='y',axis=alt.Axis(ticks=False, labelAngle=0, domain=False,offset=0,title=None,labelFontStyle='bold')),
+        # y=alt.Y("CrimeType:N",sort='-x',axis=alt.Axis(title=None,)),
+        y=alt.Y("Rate",axis=None),
+        color=alt.Color("Rate:Q", sort='y',
+                        scale=alt.Scale(scheme='goldred'),
+                        legend=None),   
+    ).properties(
+        width=250,
+        height=200,
+    ).add_selection(
+        # city_selection, year_selection
+    )
+    text_num = bars.mark_text(
+        align='center',
+        baseline='middle',
+        fontStyle='bold',
+        color='black',
+        dy=-5,
+    ).encode(
+        # x=alt.value(5),
+        # text='RentalPrice',
+        text='label:N',
+        color=alt.Color()
+    ).transform_calculate(label=f'format(datum.Rate,"d")')
+    graph_crime_detail = bars+text_num
+    colgraph1, colgraph2 = st.columns(2)
+    with colgraph1:
+        st.write(graph_rentalprice_detail)
+    with colgraph2:
+        st.write(graph_crime_detail)
+    
     
     
     city = 'New York'
