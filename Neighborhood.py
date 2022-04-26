395f31ab463b905594bffe178ce97cd9a4c520b3
jacoblipech <>
2022-04-26 09:43:15 -0400

    update visualizations

--- Neighborhood.py
+++ Neighborhood.py
@@ -16,7 +16,7 @@ alt.data_transformers.enable('default', max_rows=None)
 
 top10cities = set(["Boston", "Chicago", "Detroit", "Los Angeles", "New York", "Philadelphia", "Pittsburgh", "San Francisco", "Seattle", "Washington"])
 
-bedroomTypes = set(["1br", "2br", "3br", "4br", "5br", "condo"])
+bedroomTypes = {"1 bedroom": "1br", "2 bedrooms" : "2br", "3 bedrooms" :"3br", "4 bedrooms" : "4br", "5 bedrooms" : "5br", "Condominium" : "condo"}
 
 def readCsv(fileName):
   return pd.read_csv(fileName)
@@ -24,9 +24,9 @@ def readCsv(fileName):
 
 ################## geoJson section ##################
 city_neighborhood_mapping = {
-	"Boston": "Neighborhood_ID",
-	"SanFrancisco": "nhood",
-	"NewYork": "ntaname",
+  "Boston": "Neighborhood_ID",
+  "SanFrancisco": "nhood",
+  "NewYork": "ntaname",
 }
 
 def addLatLonGeometry(df):
@@ -45,8 +45,8 @@ def readGeoFile(fileName):
 def showGraphChart(geoDf, labelName='nhood'):
   # chart object
   graph1 = alt.Chart(geoDf).mark_geoshape(
-  	fill='lightgray',
-	stroke='white'
+    fill='lightgray',
+  stroke='white'
   ).encode(
       color=labelName,
       tooltip=labelName,
@@ -67,9 +67,52 @@ def showGraphChart(geoDf, labelName='nhood'):
   return graph1
 
 ################## city level section ##################
-# filter df by city
-def filterByCity(df, city):
-  return df[df['City'] == city]
+# lines for covid
+covidLine = alt.Chart(pd.DataFrame({'x': ['2020']})).mark_rule(color='purple').encode(x='x:T')
+postCovidLine = alt.Chart(pd.DataFrame({'x': ['2021']})).mark_rule(color='orange').encode(x='x:T')
+
+# text for pandemic
+prepandemicText = alt.Chart(pd.DataFrame({'x': ['2019-01'], 'y': ['5250']})).mark_text(text='Pre-pandemic').encode(x='x:T', y='y:Q')
+pandemicText = alt.Chart(pd.DataFrame({'x': ['2020-07'], 'y': ['5250']})).mark_text(text='Pandemic').encode(x='x:T', y='y:Q')
+postpandemicText = alt.Chart(pd.DataFrame({'x': ['2021-08'], 'y': ['5250']})).mark_text(text='Post-pandemic').encode(x='x:T', y='y:Q')
+
+
+def getCovidMarkings():
+  return covidLine + postCovidLine
+
+def getCovidText():
+  return prepandemicText + pandemicText + postpandemicText
+
+
+def showGeneralNeighborhoodChart(df):
+  mostExNeighborhoods = getTopNeighborhoods(df, 5)
+  cheapestNeighborhoods = getBottomNeighborhoods(df, 5)
+
+  topNeighborsChart = alt.Chart(df).mark_line().encode(
+    x=alt.X('Date:T'),
+    y=alt.Y('Prices:Q', scale=alt.Scale(zero=False)),
+    color=alt.Color('Neighborhood'),
+    tooltip=['Neighborhood', 'Prices']
+  ).transform_filter(
+      alt.FieldOneOfPredicate(field='Neighborhood', oneOf=cheapestNeighborhoods + mostExNeighborhoods)
+  ).properties(
+      width=600, height=300,
+      title='Top 5 most expensive and cheapest neighborhooods for rents'
+  )
+
+  st.altair_chart(topNeighborsChart + getCovidMarkings() + getCovidText())
+
+def showGeneralNeighborhoodPriceChart(df):
+  barChart = alt.Chart(df).mark_bar().encode(
+    alt.X("Date:T"),
+    alt.Y("MeanPriceChange:Q", axis=alt.Axis(format='.0%')),
+    color=alt.condition(
+        alt.datum.MeanPriceChange > 0,
+        alt.value("red"),  # The positive color
+        alt.value("green")  # The negative color
+    )
+  ).properties(width=600).interactive()
+  st.altair_chart(barChart + getCovidMarkings())
 
 
 ################## neighborhood level section ##################
@@ -88,57 +131,51 @@ def getBottomNeighborhoods(df, num):
   return df.drop_duplicates(subset=['MeanPrices']).nsmallest(num, ['MeanPrices'])['Neighborhood'].tolist()
 
 
-
-# lines for covid
-covidLine = alt.Chart(pd.DataFrame({'x': ['2020']})).mark_rule(color='red').encode(x='x:T')
-postCovidLine = alt.Chart(pd.DataFrame({'x': ['2021']})).mark_rule(color='orange').encode(x='x:T')
-
-# text for pandemic
-prepandemicText = alt.Chart(pd.DataFrame({'x': ['2019-01'], 'y': ['5250']})).mark_text(text='Pre-pandemic').encode(x='x:T', y='y:Q')
-pandemicText = alt.Chart(pd.DataFrame({'x': ['2020-07'], 'y': ['5250']})).mark_text(text='Pandemic').encode(x='x:T', y='y:Q')
-postpandemicText = alt.Chart(pd.DataFrame({'x': ['2021-08'], 'y': ['5250']})).mark_text(text='Post-pandemic').encode(x='x:T', y='y:Q')
-
-# line chart for top neighborhood
-# alt.Chart(temp_df).mark_line().encode(
-#     x=alt.X('Date:T'),
-#     y=alt.Y('Prices:Q', scale=alt.Scale(zero=False)),
-#     color=alt.Color('Neighborhood'),
-#     tooltip=['Neighborhood', 'Prices']
-# ).transform_filter(
-#     alt.FieldOneOfPredicate(field='Neighborhood', oneOf=topNeighbors + bottomNeighbors)
-# ).properties(
-#     width=600, height=300,
-#     title='Top 5 most expensive and cheapest neighborhooods for rents'
-# )
-
+################## general functions section to call other graphs ##################
 def visualizeCity(city):
-	st.write('showing city:', city)
-	# load geojson data from city
-	geoFileName = f"GeojsonData/{city}Neighborhoods.geojson"
-	# geoFileName = f"ShpData/{city}Neighborhoods.shp"
-	geoData = readGeoFile(geoFileName)
-	st.write(geoData.sample(5))
+  st.write('showing city:', city)
+  # load geojson data from city
+  geoFileName = f"GeojsonData/{city}Neighborhoods.geojson"
+  # geoFileName = f"ShpData/{city}Neighborhoods.shp"
+  geoData = readGeoFile(geoFileName)
+  st.write(geoData.sample(5))
 
-	geoData.plot("nhood", figsize = (20, 10)) # legend=True,
-	for _, row in geoData.iterrows():
-		plt.text(s=row['nhood'], x = row['lon'], y = row['lat'],
-		       horizontalalignment='center', fontdict = {'size': 10}) # 'weight': 'bold', 
+  geoData.plot("nhood", figsize = (20, 10)) # legend=True,
+  for _, row in geoData.iterrows():
+    plt.text(s=row['nhood'], x = row['lon'], y = row['lat'],
+           horizontalalignment='center', fontdict = {'size': 10}) # 'weight': 'bold', 
 
-		# plt.text(s='Data: ' + f'{hue:,}', x=row['coords'][0],y = row['coords'][1] - 0.01 ,
-		#       horizontalalignment='center', fontdict = {'size': 8})
-	st.labels = False
-	st.pyplot()
+    # plt.text(s='Data: ' + f'{hue:,}', x=row['coords'][0],y = row['coords'][1] - 0.01 ,
+    #       horizontalalignment='center', fontdict = {'size': 8})
+  st.labels = False
+  st.pyplot()
 
-	df = pd.DataFrame(geoData, columns=['lat', 'lon'])
-	st.map(df)
+  df = pd.DataFrame(geoData, columns=['lat', 'lon'])
+  st.map(df)
 
-	# st.write(geoData.head())
-	# st.altair_chart(showGraphChart(geoData, city_neighborhood_mapping[city]))
-	# st.write('ab')
+  # st.write(geoData.head())
+  # st.altair_chart(showGraphChart(geoData, city_neighborhood_mapping[city]))
+  # st.write('ab')
 
+
+def visualizeCityBedroomType(city, bedroom):
+  df = readCsv(f"data/{city}/{bedroom}Rental")
+  showGeneralNeighborhoodChart(df)
+  # TODO: add this to data preprocessing column
+  df['Price_change'] = df['Prices'].pct_change()
+  df['MeanPriceChange'] = df.groupby('Date')['Price_change'].transform('mean')
+  showGeneralNeighborhoodPriceChart(df)
 
 
 # function to load all the data and interactions
 def loadNeighborhoodData():
-	citySelection = st.selectbox("Which city would you like to see?", top10cities)
-	visualizeCity(citySelection.replace(' ', ''))
+  citySelection = st.selectbox("Which city would you like to see?", top10cities)
+  citySelection = citySelection.replace(' ', '')
+  # visualizeCity(citySelection)
+
+  st.subheader("General City Bedroom type visualizations")
+  bedroomSelection = st.selectbox("Which bedroom type would you like to see?", bedroomTypes.keys())
+  visualizeCityBedroomType(citySelection, bedroomTypes[bedroomSelection])
+
+  st.subheader("Specific neighborhood visualizations")
+  
